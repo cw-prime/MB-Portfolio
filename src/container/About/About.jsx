5fdabd01c53a663e5b9832058b85da200d72dbec
Mark B <cw.prime@gmail.com>
2024-02-18 18:11:36 -0600

    update deploy

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -31,7 +31,8 @@ const About = (props) => { // Correctly receive props as an object
         <motion.div className="slogan-container">
           <h1 className="slogan">
             <span className="slogan-primary">Innovation Unleashed, This lets this</span>
-            <span className="slogan-secondary"> Designing Here Tomorrow.</span>
+            <span className="slogan-secondary"> Designing Here Tomorrows here.</span>
+
           </h1>
         </motion.div>
         <div className="new-sections-container">
